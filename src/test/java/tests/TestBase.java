a91cc0d57e9f125f69649e5688b5f165c1718d9f
inavernitskaya <inavernitskaya@gmail.com>
2023-05-05 22:42:45 +0300

    data+task

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,18 +1,38 @@
 package tests;
 
 import manage.ApplicationManager;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class TestBase {
-    static ApplicationManager app = new ApplicationManager();
+import java.lang.reflect.Method;
 
-    @BeforeSuite
+public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+    static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    @BeforeMethod(alwaysRun = true)
+    public void startLogger(Method method){
+        logger.info("Tests start---"+method.getName());
+    }
+
+    @BeforeSuite(alwaysRun = true)
     public void setUp(){
         app.init();
     }
 
+    @AfterSuite(alwaysRun = true)
     public void stop(){
         app.quit();
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void finishLog(){
+        logger.info("********************************************************************************************************");
     }
 }
